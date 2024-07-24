66cad07fe70856114e69eca5b20b864324cdc6a5
waraumbach <waraumbach@gmail.com>
2024-07-24 15:37:07 +0200

    added port index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('hello world');
+import express from 'express'
+import 'dotenv/config'
+import mongoose from 'mongoose'
+import cors from 'cors'
+export const app = express()
+
+const PORT = process.env.PORT || 3000
+const MONGO_URI = process.env.MONGO_URI
+
+
+
+const corsOptions = {
+    exposedHeaders: ['Content-Range'],
+  };
+
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+app.use(cors(corsOptions))
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my e-commerce API')
+})
+
+
+mongoose.connect(MONGO_URI)
+    .then(() => app.listen(PORT , () => console.log(`Server is running on port ${PORT} 🟢`)))
+    .catch(err => console.error(`${err} 🔴`))
