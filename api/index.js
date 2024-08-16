d985094672a47a830170fcb07416c424168ed6e8
waraumbach <waraumbach@gmail.com>
2024-08-16 17:27:12 +0700

    update vercel

--- api/index.js
+++ api/index.js
@@ -45,4 +45,4 @@ mongoose.connect(MONGO_URI)
     .then(() => app.listen(PORT , () => console.log(`Server is running on port ${PORT} 🟢`)))
     .catch(err => console.error(`${err} 🔴`))
 
-// module.exports = app;
+module.exports = app;
